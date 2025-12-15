6dad31ab37260b51a4c566bce395df72d379876a
Quietskill1 <ravdeepbhogal@gmail.com>
2025-12-15 13:55:28 +0530

    Update and rename task2.c to task02.c

--- task02.c
+++ task02.c
@@ -3,8 +3,14 @@
 int main(void)
 {
     int s;
+    input:
     printf("Enter time in seconds: ");
-    scanf("%d",&s);
+    if (scanf("%d", &s)!=1||s<0)
+    {
+        printf("Invalid input. Please enter a non-negative integer.\n");
+        while(getchar()!='\n');
+        goto input;
+    }
     printf("Time: %d hours, %d minutes, %d seconds",s/3600,(s%3600)/60,s%60);
     return 0;
 }
