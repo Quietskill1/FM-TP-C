f3c854421af09342c9f1b118f8fa8c8483958c30
Quietskill1 <ravdeepbhogal@gmail.com>
2025-12-17 21:40:11 +0530

    Update and rename task4.c to task04.c

---
+++ task04.c
@@ -0,0 +1,55 @@
+//Create a program to remove all duplicates from an array of integers.
+//Basics of arrays, searching, shifting elements.
+#include <stdio.h>
+void accept(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        input:
+        printf("Enter element %d: ",i+1);
+        if(scanf("%d",&arr[i])!=1)
+        {
+            printf("Invalid input. Please enter an integer.\n");
+            while(getchar()!='\n');
+            goto input;
+        }
+    }
+}
+int removeDuplicates(int arr[],int n)
+{
+    int i,j,k;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;)
+        {
+            if(arr[i]==arr[j])
+            {
+                for(k=j;k<n-1;k++)
+                    arr[k]=arr[k+1];
+                n--;
+            }
+            else
+                j++;
+        }
+    }
+    return n;
+}
+int main(void)
+{
+    int n;
+    printf("Enter number of elements: ");
+    input:
+    if(scanf("%d",&n)!=1||n<=0)
+    {
+        printf("Invalid input. Please enter a positive integer.\n");
+        while(getchar()!='\n');
+        goto input;
+    }
+    int arr[n];
+    accept(arr,n);
+    n=removeDuplicates(arr,n);
+    printf("Array after removing duplicates: \n");
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+    return 0;
+}
