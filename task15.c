832d78f92ac75599f34f700c15458a7a5540fd27
Quietskill1 <ravdeepbhogal@gmail.com>
2025-12-17 23:35:40 +0530

    Update task15.c

--- task15.c
+++ task15.c
@@ -1,20 +1,28 @@
 //Create a recursive function to find the nth term of the Fibonacci series.
-// Fibonacci series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...(sum of prev two terms)
+// Fibonacci series:...-8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...(sum of prev two terms)
 #include <stdio.h>
-int fibonacci(int n) 
+int fibonacci(int n)//works with negative numbers as well
 {
-    if (n<=0)
+    if(n==0)
         return 0;
-    else if (n==1)
+    else if(n==1 || n==-1)
         return 1;
+    else if(n>1)
+        return fibonacci(n-1)+fibonacci(n-2);
     else
-        return fibonacci(n-1) + fibonacci(n-2);
+        return fibonacci(n+2)-fibonacci(n+1);
 }
 int main(void) 
 {
     int n;
     printf("Enter the term number to find in Fibonacci series: ");
-    scanf("%d", &n);
+    input:
+    if(scanf("%d", &n)!=1)
+    {
+        printf("Invalid input. Please enter an integer.\n");
+        while(getchar()!='\n');
+        goto input;
+    }
     int result = fibonacci(n);
     printf("The %dth term in the Fibonacci series is: %d\n", n, result);
     return 0;
